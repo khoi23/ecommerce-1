4cf67694f13b84ad68ee44ac0a9ee6f180ea0a36
khoi23 <101267249+khoi23@users.noreply.github.com>
2022-03-26 11:05:57 +0700

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,6 +104,28 @@ window.addEventListener('scroll', scrollUp)
 
 
 /*=============== QUESTIONS ACCORDION ===============*/
+const accordionItem = document.querySelectorAll('.questions__item')
 
+accordionItem.forEach((item) => {
+    const accordionHeader = item.querySelector('.questions__header')
 
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.questions__content')
+
+    if(item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }
+    else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
 /*=============== STYLE SWITCHER ===============*/
